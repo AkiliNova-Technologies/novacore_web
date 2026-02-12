4a684758bdd3493b9b3bba61c4be348cb4a64ea5
albert2Moon <albertwatbin@gmail.com>
2026-02-12 14:56:32 +0300

    feat: Netilfy build error correction

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   images: {
-    domains: [
-      'images.unsplash.com'
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Add other domains here if needed
     ],
   },
 };
